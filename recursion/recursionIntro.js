66f1cd463ff623c9522a86865b807a059d305680
awarenales <awarenales@gmail.com>
2019-04-07 12:56:18 -0300

    recursion exercise solving

--- recursion/recursionIntro.js
+++ recursion/recursionIntro.js
@@ -1,11 +1,68 @@
-//1. Write a function that loops through the numbers n down to 0. If you haven't done so try using a while loop to do this.
+// 1. Write a function that loops through the numbers n down to 0. If you haven't done so try using a while loop to do this.
+var countDown = function(n) {
+	while (n>=0) {
+		console.log(n);
+		n--;
+	}
+}
 
-//2. Next, try looping just like above except using recursion
+// 2. Next, try looping just like above except using recursion
+var countDownRec = function(n) {
+	if (n<0) {
+		return 'finished';
+	} else {
+		console.log(n);
+		return countDownRec(n-1);
+	}
+}
 
-//3.Write a function 'exponent' that takes two arguments base, and expo, uses a while loop to return the exponenet value of the base.
+// 3.Write a function 'exponent' that takes two arguments base, and expo, uses a while loop to return the exponenet value of the base.
+var exponent = function(base, expo) {
+	var result = 1;
+	var n = 1;
 
-//4. Write a function 'RecursiveExponent' that takes two arguments base, and expo, recursively returns exponent value of the base.
+	while (n <= expo) {
+		result = result*base;
+		n++;
+	}
 
-//5. Write a function 'recursiveMultiplier' that takes two arguments, 'arr and num', and multiplies each arr value into by num and returns an array of the values.
+	console.log(base + '^' + expo + '=' + result);
+}
 
-//6. Write a function 'recursiveReverse' that takes an array and uses recursion to return its contents in reverse
+// 4. Write a function 'RecursiveExponent' that takes two arguments base, and expo, recursively returns exponent value of the base.
+var RecursiveExponent = function(base,expo) {
+	if (expo === 1) {
+		return base;
+	} else {
+		return base*RecursiveExponent(base,expo-1);
+	}
+}
+
+// 5. Write a function 'recursiveMultiplier' that takes two arguments, 'arr and num', and multiplies each arr value into by num and returns an array of the values.
+var recursiveMultiplier = function(arr,num)  {
+	if (arr.length === 1) {
+		return [arr[0]*num];
+	} else {
+		console.log(arr.slice(1));
+		return [arr[0]*num].concat(recursiveMultiplier(arr.slice(1),num));
+	}
+}
+
+// 6. Write a function 'recursiveReverse' that takes an array and uses recursion to return its contents in reverse
+var recursiveReverse = function(arr) {
+	if (arr.length === 1) {
+		return arr;
+	} else if (arr.length === 2) {
+		return [arr[arr.length-1],arr[0]];
+	} else {
+		// return last + reverse(middle) + first
+		return [arr[arr.length-1],...recursiveReverse(arr.slice(1,arr.length-1)),arr[0]];
+	}
+}
+
+// countDown(10);
+// countDownRec(10);
+// exponent(2,3);
+// console.log(RecursiveExponent (3,3));
+// console.log(recursiveMultiplier([1,2,3,4,5],2));
+console.log(recursiveReverse([1,2,3,4,5]));
