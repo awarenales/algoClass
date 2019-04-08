187125572e3a3e901eafa2130f8aa0fbba1cab37
awarenales <awarenales@gmail.com>
2019-04-08 14:59:04 -0300

    added time complexity

--- recursion/recursionIntro.js
+++ recursion/recursionIntro.js
@@ -5,6 +5,7 @@ var countDown = function(n) {
 		n--;
 	}
 }
+// Time complexity: O(n)
 
 // 2. Next, try looping just like above except using recursion
 var countDownRec = function(n) {
@@ -15,6 +16,7 @@ var countDownRec = function(n) {
 		return countDownRec(n-1);
 	}
 }
+// Time complexity: O(n)
 
 // 3.Write a function 'exponent' that takes two arguments base, and expo, uses a while loop to return the exponenet value of the base.
 var exponent = function(base, expo) {
@@ -28,6 +30,7 @@ var exponent = function(base, expo) {
 
 	console.log(base + '^' + expo + '=' + result);
 }
+// Time complexity: O(n)
 
 // 4. Write a function 'RecursiveExponent' that takes two arguments base, and expo, recursively returns exponent value of the base.
 var RecursiveExponent = function(base,expo) {
@@ -37,6 +40,7 @@ var RecursiveExponent = function(base,expo) {
 		return base*RecursiveExponent(base,expo-1);
 	}
 }
+// Time complexity: O(n)
 
 // 5. Write a function 'recursiveMultiplier' that takes two arguments, 'arr and num', and multiplies each arr value into by num and returns an array of the values.
 var recursiveMultiplier = function(arr,num)  {
@@ -47,18 +51,20 @@ var recursiveMultiplier = function(arr,num)  {
 		return [arr[0]*num].concat(recursiveMultiplier(arr.slice(1),num));
 	}
 }
+// Time complexity: O(n)
 
 // 6. Write a function 'recursiveReverse' that takes an array and uses recursion to return its contents in reverse
 var recursiveReverse = function(arr) {
 	if (arr.length === 1) {
-		return arr;
+		return arr; // O(1)
 	} else if (arr.length === 2) {
-		return [arr[arr.length-1],arr[0]];
+		return [arr[arr.length-1],arr[0]]; // O(1)
 	} else {
 		// return last + reverse(middle) + first
-		return [arr[arr.length-1],...recursiveReverse(arr.slice(1,arr.length-1)),arr[0]];
+		return [arr[arr.length-1],...recursiveReverse(arr.slice(1,arr.length-1)),arr[0]]; 
 	}
 }
+// Time complexity: O(n/2)
 
 // countDown(10);
 // countDownRec(10);
