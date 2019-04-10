1a841fd5fcee7218661ac25334c01fc8856d986c
awarenales <awarenales@gmail.com>
2019-04-10 12:11:24 -0300

    bubblesort solutions

--- sorting-algorithms/bubble.js
+++ sorting-algorithms/bubble.js
@@ -31,3 +31,84 @@ bubblesort(array with size n) {
 	}
 }
 */
+
+var bubbleSort1 = function(arr) {
+	let count = 0;
+	for (let i = 1; i <= arr.length; i++) {
+		for (let n = 0; n < arr.length; n++) {
+			count++; // big-o helper; how many times it was executed?
+			console.log("i = " + i + ", n = " + n);
+			console.log("check: " + arr[n] + ">" + arr[n+1] + "?");
+			if (arr[n] > arr[n+1]) { // doesn't swap if equal
+				let temp = arr[n];
+				arr[n] = arr[n+1];
+				arr[n+1] = temp;
+				console.log("after: " + arr + "\n");
+			} else {
+				console.log("no swap\n");
+			}
+		}
+	}
+	console.log("*****\nSORTED ARRAY: ");
+	console.log(arr);
+	console.log(count);
+}
+// Time complexity: O(n²)
+// not adaptative: it will check the whole array at each iteraction, even if it was already checked and sorted - worst case
+
+var bubbleSort2 = function(arr) {
+	let unsorted = arr.length; // keep track of sorted itens
+	let count = 0;
+	for (let i = 1; i <= arr.length; i++) {
+		for (let n = 0; n < unsorted-1; n++) {
+			count++; // big-o helper; how many times it was executed?
+			console.log("unsorted = " + unsorted);
+			console.log("i = " + i + ", n = " + n);
+			console.log("check: " + arr[n] + ">" + arr[n+1] + "?");			
+			if (arr[n] > arr[n+1]) { // doesn't swap if equal
+				console.log("SWAP!\nbefore: " + arr);
+				let temp = arr[n];
+				arr[n] = arr[n+1];
+				arr[n+1] = temp;
+				console.log("after: " + arr + "\n");
+			} else {
+				console.log("no swap\n");
+			}
+		}
+		unsorted--;
+	}
+	console.log("*****\nSORTED ARRAY: ");
+	console.log(arr);
+	console.log(count);
+}
+// Time complexity: O(n²/2)
+
+var bubbleSort = function(arr) {
+	let unsorted = arr.length; // keep track of sorted itens
+	let count = 0;
+	for (let i = 1; i <= arr.length; i++) {
+		for (let n = 0; n < unsorted-1; n++) {
+			count++; // big-o helper; how many times it was executed?
+			console.log("unsorted = " + unsorted);
+			console.log("i = " + i + ", n = " + n);
+			console.log("check: " + arr[n] + ">" + arr[n+1] + "?");			
+			if (arr[n] > arr[n+1]) { // doesn't swap if equal
+				console.log("SWAP!\nbefore: " + arr);
+				let temp = arr[n];
+				arr[n] = arr[n+1];
+				arr[n+1] = temp;
+				console.log("after: " + arr + "\n");
+			} else {
+				console.log("no swap\n");
+			}
+		}
+		unsorted--;
+	}
+	console.log("*****\nSORTED ARRAY: ");
+	console.log(arr);
+	console.log(count);
+}
+// Time complexity: O(n²/2)
+
+// RUN
+bubbleSort1([5,3,67,3,1,7]);
