72089b14a47a4a835acb5fa2b2c82197fde04352
awarenales <awarenales@gmail.com>
2019-04-10 12:15:52 -0300

    bubblesort comments and started cocktail sort

--- sorting-algorithms/bubble.js
+++ sorting-algorithms/bubble.js
@@ -82,18 +82,55 @@ var bubbleSort2 = function(arr) {
 	console.log(count);
 }
 // Time complexity: O(n²/2)
+// won't go over the end, if it was already checked; cuts O in half; still not adaptative
+// could be better if we actually checked data for ordered pieces, to cut time if the array is partially sorted; best case would be running it on a sorted array, when O(n)
 
 var bubbleSort = function(arr) {
 	let unsorted = arr.length; // keep track of sorted itens
+	let dynamicCheck = unsorted; // keep track of sorted itens as data is checked for swap
 	let count = 0;
+
 	for (let i = 1; i <= arr.length; i++) {
 		for (let n = 0; n < unsorted-1; n++) {
 			count++; // big-o helper; how many times it was executed?
 			console.log("unsorted = " + unsorted);
+			console.log("dynCheck = " + dynamicCheck);
 			console.log("i = " + i + ", n = " + n);
-			console.log("check: " + arr[n] + ">" + arr[n+1] + "?");			
+			console.log("check: " + arr[n] + ">" + arr[n+1] + "?");
 			if (arr[n] > arr[n+1]) { // doesn't swap if equal
+				dynamicCheck = unsorted;
 				console.log("SWAP!\nbefore: " + arr);
+				let temp = arr[n];
+				arr[n] = arr[n+1];
+				arr[n+1] = temp;
+				console.log("after: " + arr + "\n");
+			} else {
+				console.log("no swap\n");
+				dynamicCheck--;
+			}
+		}
+		unsorted = dynamicCheck;
+		unsorted--;
+	}
+	console.log("*****\nSORTED ARRAY: ");
+	console.log(arr);
+	console.log(count);
+}
+// Time complexity: O(n²/2)
+// adaptative, in best case scenario, O(n)
+
+// gets max and min, sorts in both directions, from start and from end
+var cocktailSort = function (arr) {
+	let max;
+	let min;
+	let count = 0;
+
+	for (let i = 1; i <= arr.length; i++) {
+		for (let n = 0; n < arr.length; n++) {
+			count++; // big-o helper; how many times it was executed?
+			console.log("i = " + i + ", n = " + n);
+			console.log("check: " + arr[n] + ">" + arr[n+1] + "?");
+			if (arr[n] > arr[n+1]) { // doesn't swap if equal
 				let temp = arr[n];
 				arr[n] = arr[n+1];
 				arr[n+1] = temp;
@@ -102,13 +139,14 @@ var bubbleSort = function(arr) {
 				console.log("no swap\n");
 			}
 		}
-		unsorted--;
 	}
 	console.log("*****\nSORTED ARRAY: ");
 	console.log(arr);
 	console.log(count);
 }
-// Time complexity: O(n²/2)
 
 // RUN
-bubbleSort1([5,3,67,3,1,7]);
+bubbleSort([5,3,67,3,1,7]);
+
+// best case scenario
+bubbleSort([ 1, 3, 3, 5, 7, 67 ]);
